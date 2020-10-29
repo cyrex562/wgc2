a8c2fbcb55d6003e3d531bcabfd320729071e9fb
Josh M <joshmadden@occamsrazortech.com>
2020-10-29 13:12:37 -0400

    #13, #42 - wg set peer remove

--- test/test_harness.py
+++ test/test_harness.py
@@ -350,13 +350,19 @@ def test_wg_add_remove_peer(make_interface):
     post_interface: Interface = remove_peer(ifc_name, fake_peer.public_key)
     assert len(post_interface.peers) == 0
 
-
-def test_wg_remove_peer():
-    assert False
-
-
-def test_wg_set_peer_remove():
-    assert False
+def test_wg_set_peer_remove(make_interface):
+    fake_peer: Peer = gen_fake_peer()
+    ifc_name = make_interface.name
+    pre_interface: Interface = add_peer(ifc_name, fake_peer)
+    r = requests.put(f"{URL}/wg/set/{ifc_name}",
+        json={
+            "peer": {
+                "public_key": fake_peer.public_key,
+                "remove": True,
+            }
+        })
+    post_interface = process_ifc_json(r.json())
+    assert len(post_interface.peers) == 0
 
 
 def test_wg_set_peer_endpoint():
@@ -390,50 +396,5 @@ def test_wg_syncconf():
 def test_wg_import_config():
     assert False
 
-
-# def check_create_interface(app_ctx: AppContext) -> bool:
-#     ifc_name, response = create_interface(app_ctx)
-#     print(f"interface name={ifc_name}")
-#     if not response.ok():
-#         print("failed to create interface")
-#         return False
-#     return delete_interface(app_ctx, ifc_name)
-
-
-# def check_wg_set_fwmark_0(app_ctx: AppContext) -> bool:
-#     return False
-
-
-# def check_wg_set_fwmark_val(app_ctx: AppContext) -> bool:
-#     return False
-
-
-# ALL_CHECKS = {
-#     "wg_set_fwmark_0": check_wg_set_fwmark_0,
-#     "wg_set_fwmark_val": check_wg_set_fwmark_val
-# }
-
-# def run() -> int:
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument("--test","-t", required=True, choices=ALL_CHECKS.keys())
-#     parser.add_argument("--address","-a", required=True)
-#     args = parser.parse_args()
-#     test_to_run: str = args.test.lower()
-
-#     if test_to_run == "all":
-#         for check in ALL_CHECKS.values():
-#             result = check()
-#             print(f"check={check.__name__}, result={result}")
-#     elif ALL_CHECKS.get(test_to_run, None) is not None:
-#         check = ALL_CHECKS[test_to_run]
-#         result = check()
-#         print(f"check={check.__name__}, result={result}")
-#     else:
-#         print(f"invalid test name={test_to_run}, valid tests={ALL_CHECKS.keys()}")
-#     return 0
-
-
-# if __name__ == "__main__":
-#     sys.exit(run())
 
 # END OF FILE
